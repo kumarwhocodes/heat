d308fc1a89264af957d56692b8c6bc5ae4452af5
Kumar Sambhav <sambhav26k@gmail.com>
2025-07-27 02:07:48 +0530

    fix: minor changes

--- src/main/java/com/zerobee/heat/dto/CustomerDTO.java
+++ src/main/java/com/zerobee/heat/dto/CustomerDTO.java
@@ -1,6 +1,8 @@
 package com.zerobee.heat.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 import java.util.List;
@@ -13,14 +15,20 @@ public class CustomerDTO {
 
     private String customerId;
     
+    @NotBlank(message = "Name is required")
     @NotNull
     private String clientName;
     @NotNull
+    @NotBlank(message = "Email is required")
     private String clientEmail;
     @NotNull
+    @NotBlank(message = "Phone Number is required")
+    @Pattern(regexp = "^[0-9]\\d{9}$", message = "Invalid phone number")
     private String clientPhone;
+    @NotBlank(message = "Nationality is required")
     @NotNull
     private String nationality;
+    @NotBlank(message = "Alternate Phone is required")
     @NotNull
     private String clientAlternatePhone;
     private String clientLanguage;
